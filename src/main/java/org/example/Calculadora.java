ce412870f6373895c1ac52dcd2553150e3f40c3a
Albert Sevy <albertstorres@gmail.com>
2024-08-01 22:07:44 -0300

    Implementado jacoco

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -10,4 +10,7 @@ public class Calculadora {
     public double multiply(double num1, double num2) {
         return num1*num2;
     }
+    public double divide(double num1, double num2) {
+        return num1 / num2;
+    }
 }
